9733961147e613c28b3da016760c3cfa3e4be2d8
yorushima <sabitova.regina2017@mail.ru>
2024-05-09 17:21:56 +0500

    ...

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -11,25 +11,39 @@ import styles from '../styles/shop.module.css';
 const Shop = () => {
   return (
     <div className={styles.shop}>
-        <Carousel>
-          <Carousel.Item>
-          <img className={styles.images} src={IMAGE} alt="First slide" />
-            <Carousel.Caption></Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-          <img className={styles.images} src={IMAGE} alt="Second slide" />
-            <Carousel.Caption></Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-          <img className={styles.images} src={IMAGE} alt="Third slide" />
-            <Carousel.Caption></Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-        <p className={styles.why_us_head}>Почему мы?</p>
-        <div className={styles.why_us}>
-          
+      <Carousel>
+        <Carousel.Item>
+        <img className={styles.images} src={IMAGE} alt="First slide" />
+          <Carousel.Caption></Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+        <img className={styles.images} src={IMAGE} alt="Second slide" />
+          <Carousel.Caption></Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+        <img className={styles.images} src={IMAGE} alt="Third slide" />
+          <Carousel.Caption></Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      <p className={styles.why_us_head}>Почему мы?</p>
+      <div className={styles.why_us}>
+        <div className={styles.why_us_columns}>
+          <img src={IMAGE} alt='///' />
+          <p>Большой опыт работы в сфере</p>
+          <p>***</p>
         </div>
-        
+        <div className={styles.why_us_columns}>
+          <img src={IMAGE} alt='///' />
+          <p>Официальные дистрибьютеры</p>
+          <p>***</p>
+        </div>
+        <div className={styles.why_us_columns}>
+          <img src={IMAGE} alt='///' />
+          <p>Широкий выбор ассортимента</p>
+          <p>***</p>
+        </div>
+      </div>
+      
     </div>
   );
 }
