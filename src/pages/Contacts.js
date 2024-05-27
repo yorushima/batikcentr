469b42f53ecc8eedf93ffb4cb77b581d3dd5d72e
yorushima <sabitova.regina2017@mail.ru>
2024-05-27 19:47:11 +0500

    ///

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import styles from '../styles/shop.module.css';
+
+import IMAGE from '../img/call.svg';
+
 const Contacts = () => {
   return (
-    <div>
-        Contacts
+    <div className={styles.contacts}>
+        <img src={IMAGE} alt='Call'/>
+        <p>8-912-601-06-60</p>
+        <p>8(343)266-48-80</p>
     </div>
   );
 }
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
+import Slider from "react-slick";
 
 // import IMAGE from '../img/placeholder.svg';
 import IMAGE from '../img/black.jpg';
 
 import styles from '../styles/shop.module.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 
 
 const Shop = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  };
   return (
     <div className={styles.shop}>
       <Carousel>
@@ -55,8 +65,40 @@ const Shop = () => {
         <img src={IMAGE} alt='///' />
         <button>узнать подробнее в наших соцсетях...</button>
       </div>
-      <div className={styles.reviews}>
       <h2 className={styles.head}>Отзывы наших покупателей</h2>
+      <div className={styles.reviews}>
+        <div className="slider-container">
+          <Slider {...settings}>
+            <div>
+              <h3><img src={IMAGE} alt='1' /></h3>
+            </div>
+            <div>
+              <h3><img src={IMAGE} alt='2' /></h3>
+            </div>
+            <div>
+              <h3><img src={IMAGE} alt='3' /></h3>
+            </div>
+            <div>
+              <h3><img src={IMAGE} alt='4' /></h3>
+            </div>
+            <div>
+              <h3><img src={IMAGE} alt='5' /></h3>
+            </div>
+            <div>
+              <h3><img src={IMAGE} alt='5' /></h3>
+            </div>
+            {/* <div>
+              <h3>7</h3>
+            </div>
+            <div>
+              <h3>8</h3>
+            </div>
+            <div>
+              <h3>9</h3>
+            </div> */}
+          </Slider>
+        </div>
+        <button className={styles.button}>смотреть ещё</button>
       </div>
     </div>
   );
